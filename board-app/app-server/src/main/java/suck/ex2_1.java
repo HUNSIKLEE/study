252de4391bf0385fabf8a494d6cac17a3b9c6b21
sik <hunsik0206@gmail.com>
2022-09-25 19:48:33 +0900

    add:core

---
+++ board-app/app-server/src/main/java/suck/ex2_1.java
@@ -0,0 +1,13 @@
+package suck;
+
+public class ex2_1 {
+    public static void main(String[] args){
+
+        int year = 0 ; // Stack 메모리에 year 0으로 초기화한 변수가 생성된다.
+        int age = 14; //  Stack 메모리에 age 정수 14를 포함한 변수가 생성된다.
+
+        System.out.println(year); // 0이 출력된다.
+        System.out.println(age); //  14가 출력된다.
+    }
+
+}
---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(long memberId) {
+        return store.get(memberId);
+    }
+}
