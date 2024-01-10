a598fc974e1078351012f19aa25c2c0fc9c636f2
{HUNSIKLEE} <hunsik0206@gmail.com>
2024-01-10 23:22:16 +0900

    feat: class

---
+++ java-basic/src/class1/ClassStart2.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class ClassStart2 {
+
+    public static void main(String[] args) {
+
+        String[] studentNames = {"학생1", "학생2", "학생3","학생4"};
+        int[] studentAges = {15, 16, 17, 18};
+        int[] studentGrade = {90, 80, 70, 60};
+
+        for (int i = 0; i < studentNames.length; i++){
+            System.out.println("이름:"  + studentNames[i] + "나이" + studentAges[i] + "성적:" + studentGrade[i]);
+        }
+    }
+}
