cd851738f3647e3057fa7e8898de8052965a9188
HUNSIKLEE <hunsik0206@gmail.com>
2024-04-09 23:36:37 +0900

    feat: jdbc

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -2,15 +2,13 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class MemoryMemberRepositoryTest {
+public class   MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
