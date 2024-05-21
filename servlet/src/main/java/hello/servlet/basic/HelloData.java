0cc1a5842872abeeca9e303ecc5483c0f98529d0
HUNSIKLEE <hunsik0206@gmail.com>
2024-05-21 14:55:51 +0900

    feat:servlet

---
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private System username;
+    private int age;
+
+}
